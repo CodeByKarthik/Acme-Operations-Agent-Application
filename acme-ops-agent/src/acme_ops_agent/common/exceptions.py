a5d1e2a36577da634927ed385486ae400469b4f8
Karthikeyan Sivakumar <126179797+Karthi-DStech@users.noreply.github.com>
2026-05-28 18:28:55 +0100

    Added Exceptions

---
+++ acme-ops-agent/src/acme_ops_agent/common/exceptions.py
@@ -0,0 +1,13 @@
+class AuthError(Exception):
+    """
+    Raised when authentication fails.
+    """
+
+
+class PermissionDenied(Exception):
+    """
+    Raised when authorization fails.
+    """
+
+class AppUserNotFoundError(Exception):
+    pass
